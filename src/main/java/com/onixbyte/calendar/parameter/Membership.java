015e6007aa4552403a61ba65ae1574ba79d74152
siujamo <jamo.siu@outlook.com>
2025-06-26 18:01:36 +0800

    feat: add parameter `membership`

---
+++ src/main/java/com/onixbyte/calendar/parameter/Membership.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2024-2025 OnixByte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.onixbyte.calendar.parameter;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Stream;
+
+public record Membership(
+        List<URI> members
+) {
+
+    public static Membership of(URI... members) {
+        return new Membership(List.of(members));
+    }
+
+    public static Membership of(String... members) {
+        return new Membership(Stream.of(members)
+                .map(URI::create)
+                .toList());
+    }
+
+    public String formatted() {
+        var _members = members.stream()
+                .map((member) -> '"' + member.toString() + '"')
+                .toList();
+        return "MEMBER=" + String.join(",", _members);
+    }
+}
