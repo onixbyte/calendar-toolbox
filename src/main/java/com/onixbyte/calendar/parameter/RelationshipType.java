487a865129454a2132033340bc7485d7bdebc600
siujamo <jamo.siu@outlook.com>
2025-06-27 09:14:22 +0800

    feat: add parameter `relationship type`

---
+++ src/main/java/com/onixbyte/calendar/parameter/RelationshipType.java
@@ -0,0 +1,33 @@
+package com.onixbyte.calendar.parameter;
+
+public enum RelationshipType {
+
+    CHILD,
+    PARENT,
+    SIBLING,
+    SNOOZE,
+    CONCEPT,
+    DEPENDS_ON("DEPENDS-ON"),
+    FINISH_TO_FINISH("FINISHTOFINISH"),
+    FINISH_TO_START("FINISHTOSTART"),
+    FIRST,
+    NEXT,
+    REFERENCE_ID("REFID"),
+    START_TO_FINISH("STARTTOFINISH"),
+    START_TO_START("STARTTOSTART"),
+    ;
+
+    private final String label;
+
+    RelationshipType() {
+        this.label = name();
+    }
+
+    RelationshipType(String label) {
+        this.label = label;
+    }
+
+    public String formatted() {
+        return "RELTYPE=" + label;
+    }
+}
