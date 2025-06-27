24123a40ac3c935a1a901548634292d024a433d5
zihluwang <really@zihlu.wang>
2025-06-27 16:42:43 +0800

    feat: add property `contact`

---
+++ src/main/java/com/onixbyte/calendar/property/Contact.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2024-2025 OnixByte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.onixbyte.calendar.property;
+
+import com.onixbyte.calendar.parameter.AlternateTextRepresentation;
+import com.onixbyte.calendar.parameter.Language;
+import com.onixbyte.calendar.util.ParamAppender;
+
+public class Contact implements ComponentProperty {
+
+    private final AlternateTextRepresentation alternateTextRepresentation;
+
+    private final Language language;
+
+    private final String value;
+
+    private Contact(
+            AlternateTextRepresentation alternateTextRepresentation,
+            Language language,
+            String value
+    ) {
+        this.alternateTextRepresentation = alternateTextRepresentation;
+        this.language = language;
+        this.value = value;
+    }
+
+    public static ContactBuilder builder() {
+        return new ContactBuilder();
+    }
+
+    public static class ContactBuilder {
+        private AlternateTextRepresentation alternateTextRepresentation;
+        private Language language;
+
+        private ContactBuilder() {
+        }
+
+        public ContactBuilder withAlternateTextRepresentation(
+                AlternateTextRepresentation alternateTextRepresentation
+        ) {
+            this.alternateTextRepresentation = alternateTextRepresentation;
+            return this;
+        }
+
+        public ContactBuilder withLanguage(Language language) {
+            this.language = language;
+            return this;
+        }
+
+        public Contact build(String value) {
+            return new Contact(alternateTextRepresentation, language, value);
+        }
+    }
+
+    @Override
+    public String formatted() {
+        var builder = new StringBuilder();
+        builder.append("CONTACT");
+        ParamAppender.append(builder, alternateTextRepresentation);
+        ParamAppender.append(builder, language);
+        builder.append(":").append(value);
+        return builder.toString();
+    }
+
+
+}
