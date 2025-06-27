b7ec4a49f6382fe020f64b27f9544bafd30689a1
zihluwang <really@zihlu.wang>
2025-06-27 11:33:23 +0800

    feat: add property `priority`

---
+++ src/main/java/com/onixbyte/calendar/property/Priority.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2024-2025 OnixByte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.onixbyte.calendar.property;
+
+public class Priority implements ComponentProperty {
+
+    private final int value;
+
+    private Priority(int value) {
+        if (value < 0 || value > 9) {
+            throw new IllegalArgumentException("Property PRIORITY should between 0 and 9.");
+        }
+
+        this.value = value;
+    }
+
+    public static Priority of(int priority) {
+        return new Priority(priority);
+    }
+
+    @Override
+    public String formatted() {
+        return "PRIORITY=" + value;
+    }
+}
