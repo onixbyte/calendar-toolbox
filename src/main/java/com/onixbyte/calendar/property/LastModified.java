470c469bf94df7ebd3ade00ebcef4a0b4b797e2d
zihluwang <really@zihlu.wang>
2025-06-29 21:21:58 +0800

    feat: add property `last modified`

---
+++ src/main/java/com/onixbyte/calendar/property/LastModified.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024-2025 OnixByte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.onixbyte.calendar.property;
+
+import com.onixbyte.calendar.util.Formatters;
+
+import java.time.ZonedDateTime;
+
+public final class LastModified implements ComponentProperty {
+
+    private final ZonedDateTime value;
+
+    private LastModified(ZonedDateTime value) {
+        this.value = value;
+    }
+
+    public static DateTimeCreatedBuilder builder() {
+        return new DateTimeCreatedBuilder();
+    }
+
+    public static class DateTimeCreatedBuilder {
+        private DateTimeCreatedBuilder() {
+        }
+
+        public LastModified build(ZonedDateTime value) {
+            return new LastModified(value);
+        }
+    }
+
+    @Override
+    public String formatted() {
+        return "LAST-MODIFIED:" + value.format(Formatters.ICALENDAR_UTC_TIMESTAMP_FORMATTER);
+    }
+}
