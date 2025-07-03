9eda130d0280383d0d9fe06e51dbce841c76f837
siujamo <jamo.siu@outlook.com>
2025-07-03 09:25:00 +0800

    docs: add docs for parameter `alarm trigger relationship`

--- src/main/java/com/onixbyte/calendar/parameter/AlarmTriggerRelationship.java
+++ src/main/java/com/onixbyte/calendar/parameter/AlarmTriggerRelationship.java
@@ -1,11 +1,34 @@
 package com.onixbyte.calendar.parameter;
 
+/**
+ * Enumerates the possible relationships for the {@code RELATED} parameter in an iCalendar
+ * alarm trigger.
+ * <p>
+ * This parameter defines whether an alarm is related to the start or end of the associated
+ * calendar component.
+ * <p>
+ * Instances of this enum are formatted as specified in RFC 5545 for use in iCalendar properties.
+ *
+ * @author siujamo
+ */
 public enum AlarmTriggerRelationship implements Parameter {
 
+    /**
+     * Indicates that the alarm is related to the start of the calendar component.
+     */
     START,
+
+    /**
+     * Indicates that the alarm is related to the end of the calendar component.
+     */
     END,
     ;
 
+    /**
+     * Returns the formatted parameter string for inclusion in an iCalendar alarm trigger.
+     *
+     * @return a string formatted as {@code RELATED=START} or {@code RELATED=END}
+     */
     @Override
     public String formatted() {
         return "RELATED=" + name();
