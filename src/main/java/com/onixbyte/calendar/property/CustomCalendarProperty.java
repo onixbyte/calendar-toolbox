b6d8ea610026b0b0a58a1dda4802a1fe663aafb5
siujamo <jamo.siu@outlook.com>
2025-07-25 10:59:53 +0800

    docs: add javadoc to custom calendar property

--- src/main/java/com/onixbyte/calendar/property/CustomCalendarProperty.java
+++ src/main/java/com/onixbyte/calendar/property/CustomCalendarProperty.java
@@ -28,12 +28,16 @@ import com.onixbyte.calendar.util.PropertyComposer;
 
 import java.util.List;
 
+/**
+ * This class represents all custom properties that are not implemented and
+ * start with <code>X-</code>.
+ *
+ * @author siujamo
+ */
 public final class CustomCalendarProperty implements ComponentProperty {
 
     private final String propertyName;
-
     private final String value;
-
     private final List<Parameter> parameters;
 
     private CustomCalendarProperty(String propertyName, String value, List<Parameter> parameters) {
@@ -42,26 +46,52 @@ public final class CustomCalendarProperty implements ComponentProperty {
         this.parameters = parameters;
     }
 
+    /**
+     * Create a builder instance of <code>CustomComponentProperty</code>.
+     *
+     * @return a builder instance
+     */
     public static CustomCalendarPropertyBuilder builder() {
         return new CustomCalendarPropertyBuilder();
     }
 
+    /**
+     * Builder of <code>CustomComponentProperty</code>.
+     */
     public static class CustomCalendarPropertyBuilder {
         private List<Parameter> parameters;
 
         private CustomCalendarPropertyBuilder() {
         }
 
-        private CustomCalendarPropertyBuilder withParameters(Parameter... parameters) {
+        /**
+         * Set parameters of a calendar property.
+         *
+         * @param parameters parameter list
+         * @return builder instance
+         */
+        public CustomCalendarPropertyBuilder withParameters(Parameter... parameters) {
             this.parameters = List.of(parameters);
             return this;
         }
 
+        /**
+         * Build a <code>CustomComponentProperty</code> with given property name and value.
+         *
+         * @param propertyName  name of the property
+         * @param propertyValue value of the property
+         * @return built <code>CustomComponentProperty</code> with given name and value
+         */
         public CustomCalendarProperty build(String propertyName, String propertyValue) {
             return new CustomCalendarProperty(propertyName, propertyValue, parameters);
         }
     }
 
+    /**
+     * Output this property in <code>ics</code> format.
+     *
+     * @return <code>ics</code>-formatted string
+     */
     @Override
     public String formatted() {
         var composer = PropertyComposer.of(propertyName);
